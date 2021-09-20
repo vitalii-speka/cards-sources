a4ee01eadc1d659c3c896b51ee6470d3f2479436
vitalii-speka <65010140+vitalii-speka@users.noreply.github.com>
2021-09-20 15:58:39 +0300

    save

--- 03-slider-sources/js/app.js
+++ 03-slider-sources/js/app.js
@@ -1,36 +1,42 @@
-const upBtn = document.querySelector(".up-button");
-const downBtn = document.querySelector(".down-button");
+const upBtn = document.querySelector('.up-button');
+const downBtn = document.querySelector('.down-button');
 
-const sidebar = document.querySelector(".sidebar");
-const mainSlide = document.querySelector(".main-slide");
+const sidebar = document.querySelector('.sidebar');
 
-const slidesCount = mainSlide.querySelectorAll(".div").length;
-console.log("🚀 ~ file: app.js ~ line 8 ~ slidesCount", slidesCount);
+const container = document.querySelector('.container');
+
+const mainSlide = document.querySelector('.main-slide');
+
+const slideCount = mainSlide.querySelectorAll('div').length;
 
 let activeSlideIndex = 0;
 
-sidebar.style.top = `-${(slidesCount - 1) * 100}vh`;
+sidebar.style.top = `-${(slideCount - 1) * 100}vh`;
 
-upBtn.addEventListener("click", () => {
-  changeSlide("up");
-});
-downBtn.addEventListener("click", () => {
-  changeSlide("down");
+upBtn.addEventListener('click', () => {
+  change('up');
+  console.log('up');
 });
 
-function changeSlide(direction) {
-  if (direction === "up") {
+downBtn.addEventListener('click', () => {
+  change('down');
+  console.log('down');
+});
+
+function change(direction) {
+  if (direction === 'up') {
     activeSlideIndex++;
-    if (activeSlideIndex === slidesCount) {
+    if (activeSlideIndex === slideCount) {
       activeSlideIndex = 0;
     }
-  } else if (direction === "down") {
+  } else if (direction === 'down') {
+    activeSlideIndex--;
     if (activeSlideIndex < 0) {
-      activeSlideIndex = slidesCount - 1;
+      activeSlideIndex = slideCount - 1;
     }
   }
+
+  const height = container.clientHeight;
+  mainSlide.style.transform = `translateY(-${activeSlideIndex * height}px)`;
+  sidebar.style.transform = `translateY(${activeSlideIndex * height}px)`;
 }
-
-const height = container.clientHeight;
-
-mainSlide.style.transform = `translateY(-${activeSlideIndex * height}px)`;
